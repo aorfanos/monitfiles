1a33b69eff792bb1780d3349d9d7a4d9d41b1e28
Alexandros Orfanos <a.orfanos@acg.edu>
2019-07-07 00:22:31 +0100

    Improve installer

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ chmod +x ${PWD}/slack_mssgs/*
 ln -s ${PWD}/slack_mssgs/* /usr/local/bin/
 
 cp ${PWD}/conf-available/* /etc/monit/conf-available/ 
+cp ${PWD}/reports/report-{hourly,weekly,monthly} /etc/cron.hourly/report-{hourly,weekly,monthly}
