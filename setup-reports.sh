ceb87a19e705c4f80eb07fe4044efea7f6bf0810
Alexandros Orfanos <a.orfanos@acg.edu>
2019-07-07 01:37:38 +0100

    Fix reporting

--- setup-reports.sh
+++ setup-reports.sh
@@ -6,7 +6,10 @@ freq=(hourly daily weekly monthly)
 
 for when in "${freq[@]}"; do 
 	ln -s ${PWD}/reports/report-$when /usr/local/bin/report-$when; 
-	MONIT_EVENT=Report MONIT_DESCRIPTION=`/usr/local/bin/report-$when` \
-		MONIT_HOST=`hostname` MONIT_DATE=`date -R` MONIT_COLOR="#808080" \
-		/usr/local/bin/echo
+	sudo tee /etc/cron.$when/report-$when << EOF
+		MONIT_EVENT=Report MONIT_DESCRIPTION=`/usr/local/bin/report-$when` \
+			MONIT_HOST=`hostname` MONIT_DATE=`date -R` MONIT_COLOR="#808080" \
+			/usr/local/bin/echo
+EOF
+
 done
